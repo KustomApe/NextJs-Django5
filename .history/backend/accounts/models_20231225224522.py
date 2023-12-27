a12745aa28ba50ab2731370852fa9ea8f56acbc5
kustomApe <kustomape@gmail.com>
2023-12-28 00:16:22 +0900

    Apply new .gitignore.

--- .history/backend/accounts/models_20231225224522.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import (
-    AbstractBaseUser,
-    PermissionsMixin,
-    BaseUserManager,
-)
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from hashids import Hashids
