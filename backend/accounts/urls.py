bee4676ffdd54e520988f5630a37c1920d0ebcaa
kustomApe <kustomape@gmail.com>
2025-03-05 22:19:03 +0900

    Adds basic settings and basic practice.

--- backend/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts import views
-
-urlpatterns = [
-    # ユーザー詳細
-    path("users/<uid>/", views.UserDetailView.as_view()),
-]
--- backend/api/hello/apps.py
+++ backend/api/hello/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccountsConfig(AppConfig):
+class HelloConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "accounts"
+    name = "hello"
